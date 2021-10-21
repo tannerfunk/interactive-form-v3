74dd75566e0ae9226b1013e3a2513fb6d7ed29be
tannerfunk <88557372+tannerfunk@users.noreply.github.com>
2021-10-20 23:26:50 -0600

    IT'S WORKING!!!

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-console.log('Test');
-console.log('test2 for repo');
+console.log("test")
+
+const userName = document.getElementById("name"); // grab the element and focus it when the page loads
+userName.focus()
+console.log(userName);
+
+const userJobRole = document.getElementById("title"); //grab these other elements
+const userJobRoleOther = document.getElementById("other-job-role");
+
+console.log(userJobRole); 
+console.log(userJobRoleOther);
+
+userJobRoleOther.style.display = 'none'; //use javascript to hide it!
+
+userJobRole.addEventListener("change", (e) => {
+    if (e.target.value === 'other') {
+        userJobRoleOther.style.display = 'block';
+    } else {
+        userJobRoleOther.style.display = 'none';
+    }
+    console.log(e.target.value)
+});
+
+
